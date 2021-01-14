fa886881f9ebf1991cbcc1698e75934202e3ea65
zammy786 <57328017+zammy786@users.noreply.github.com>
2021-01-14 18:44:42 +0530

    Add files via upload

---
+++ admin/header.php
@@ -0,0 +1,9 @@
+
+
+    
+
+<div style="border:#fff 1px solid; margin:auto; margin-top:0px; background:#FF4500 url(images/templatemo_top_panel_background.jpg) no-repeat; width: 1050px;  height:90px;" align="center"> <h1 style="color:#FF450;font-size: 28px;">FIZZY</h1></div>
+
+    
+
+ 
